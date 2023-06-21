8e8f88af712b0e1d5cefff64636bec397b56ba74
Rishabh Singh <rishabhsingh812002@gmail.com>
2023-06-22 02:14:53 +0530

    Deploying the site

--- config/mongoose.js
+++ config/mongoose.js
@@ -2,7 +2,10 @@
 
 const mongoose = require('mongoose');
 // const env = require('./environment');
-mongoose.connect('mongodb://127.0.0.1:27017/todos');
+mongoose.connect('mongodb+srv://mavrishabh:RSmaverik3204%40@cluster0.ys8r5gy.mongodb.net/',{
+    useUnifiedTopology: true,
+    useNewUrlParser: true
+});
 
 const db = mongoose.connection;
 
